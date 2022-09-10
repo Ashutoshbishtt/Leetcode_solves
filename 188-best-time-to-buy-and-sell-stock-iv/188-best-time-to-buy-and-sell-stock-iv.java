d73905fe4e32c1082c3395fe631a5326fc271f87
Ashutoshbishtt <81284766+Ashutoshbishtt@users.noreply.github.com>
2022-09-10 22:51:05 +0530

    Time: 5 ms (55.67%), Space: 41.6 MB (90.32%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -4,15 +4,15 @@ class Solution {
             return 0;
         }
         
-        int[] buy = new int[k]; // buy[i]: min cost at (i + 1)-th time stock purchased
-        int[] sell = new int[k]; // sell[i]: max revenue at (i + 1)-th time stock sold
+        int[] buy = new int[k];
+        int[] sell = new int[k]; 
         Arrays.fill(buy, Integer.MIN_VALUE);
         Arrays.fill(sell, 0);
         
         for (int i : prices) {
             buy[0] = Math.max(buy[0], i * -1);
             sell[0] = Math.max(sell[0], buy[0] + i);
-            for (int j = 1; j < k; j++) { // The i-th time buy & sell depens on the (i - 1)-th time buy & sell
+            for (int j = 1; j < k; j++) { 
                 buy[j] = Math.max(buy[j], sell[j - 1] - i);
                 sell[j] = Math.max(sell[j], buy[j] + i);
             }
