d0e7628275f3ac9ea67194741bdc34bae23437f9
Ashutoshbishtt <81284766+Ashutoshbishtt@users.noreply.github.com>
2022-09-24 01:37:07 +0530

    Time: 40 ms (17.75%), Space: 72.1 MB (63.61%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[][] updateMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        for(int i = 0 ; i < matrix.length ; i++){
+            for(int j = 0 ; j < matrix[0].length ; j++){
+                if(matrix[i][j]==0)queue.add(new int[]{i,j});
+                else
+                    matrix[i][j]=Integer.MAX_VALUE;
+            }
+        }
+        
+        int[][] dirs = {{0,1},{-1,0},{1,0},{0,-1}};
+        
+        
+        
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            for (int[] d : dirs) {
+                int r = cell[0] + d[0];
+                int c = cell[1] + d[1];
+                if (r < 0 || r >= m || c < 0 || c >= n || 
+                    matrix[r][c] <= matrix[cell[0]][cell[1]] + 1) continue;
+                queue.add(new int[] {r, c});
+                matrix[r][c] = matrix[cell[0]][cell[1]] + 1;
+            }
+        }
+        
+        return matrix;
+    }
+}
