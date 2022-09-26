8a497ee803260a10bfea44e16364ced129731f09
Ashutoshbishtt <81284766+Ashutoshbishtt@users.noreply.github.com>
2022-09-26 16:04:14 +0530

    Time: 45 ms (79.85%), Space: 50.8 MB (94.70%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,27 +1,17 @@
 class Solution {
     public int longestSubarray(int[] nums, int limit) {
-        int start = 0;
-        int end = 0;
-        int res = 1;
-        
-        PriorityQueue<Integer> minQ = new PriorityQueue<>();
-        PriorityQueue<Integer> maxQ = new PriorityQueue<>(Collections.reverseOrder());
-        
-        while(start<=end && end<nums.length){
-            minQ.offer(nums[end]);
-            maxQ.offer(nums[end]);
-            
-            int minNum = minQ.peek();
-            int maxNum = maxQ.peek();
-            
-            if(maxNum - minNum <= limit){
-                end++;
-                res=Math.max(res,end-start);
-            }else{
-                minQ.remove(nums[start]);
-                maxQ.remove(nums[start]);
-                start++;
-                end++;
+        int l = 0;
+        int res = 1, max = 0, min = Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            min = Math.min(min,nums[i]);
+            max = Math.max(max,nums[i]);
+            if(max-min<=limit) {res=Math.max(res,i-l+1); continue;}
+            l = i;
+            min = max = nums[i];
+            while(l>0&&Math.abs(nums[l-1]-nums[i])<=limit){
+                l--;
+                max = Math.max(max,nums[l]);
+                min = Math.min(min,nums[l]);
             }
         }
         return res;
