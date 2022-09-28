f55515a1c148fdca83f137d512fc184485cbf8cd
Ashutoshbishtt <81284766+Ashutoshbishtt@users.noreply.github.com>
2022-09-28 23:55:20 +0530

    Time: 14 ms (5.91%), Space: 50.5 MB (5.41%) - LeetHub

---
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+       int totalSum = 0;
+        
+        for (int i = 0; i < arr.length; i += 1) {
+            for (int j = i + 1; j <= arr.length; j += 2) {
+                
+                int[] subArray = Arrays.copyOfRange(arr, i, j);
+                
+                for (int k = 0; k < subArray.length; k += 1) {
+                    
+                    totalSum += subArray[k];
+                }
+            }
+        }
+        
+        return totalSum;
+    }
+}
