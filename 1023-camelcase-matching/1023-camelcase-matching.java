b5569639cc8ce75ab29eb04f0349321d44674bcf
Ashutoshbishtt <81284766+Ashutoshbishtt@users.noreply.github.com>
2022-10-13 01:15:00 +0530

    Time: 1 ms (82.54%), Space: 42.5 MB (15.46%) - LeetHub

--- 1023-camelcase-matching/1023-camelcase-matching.java
+++ 1023-camelcase-matching/1023-camelcase-matching.java
@@ -1,20 +1,16 @@
 class Solution {
+
     public List<Boolean> camelMatch(String[] queries, String pattern) {
         List<Boolean> res = new ArrayList<>();
-        for(String query : queries)
-            res.add(isMatch(query,pattern));
+        for (String query : queries) res.add(isMatch(query, pattern));
         return res;
     }
-    
-    private boolean isMatch(String query , String pattern){
-        int i = 0 ;
-        for(char c : query.toCharArray()){
-            if(i<pattern.length() && c==pattern.charAt(i))
-                i++;
-            else if(c<'a')
-                return false;
+
+    private boolean isMatch(String query, String pattern) {
+        int i = 0;
+        for (char c : query.toCharArray()) {
+            if (i < pattern.length() && c == pattern.charAt(i)) i++; else if (c < 'a') return false;
         }
-        return i==pattern.length();
+        return i == pattern.length();
     }
-    
 }
