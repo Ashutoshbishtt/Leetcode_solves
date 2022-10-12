6de212ba2154de7a75bcf33304172403851c082d
Ashutoshbishtt <81284766+Ashutoshbishtt@users.noreply.github.com>
2022-10-12 19:59:40 +0530

    Time: 393 ms (98.89%), Space: 91.8 MB (94.73%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -7,7 +7,6 @@ public class WordDictionary {
 
     private TrieNode root = new TrieNode();
 
-    // Adds a word into the data structure.
     public void addWord(String word) {
         TrieNode node = root;
         for (char c : word.toCharArray()) {
@@ -19,8 +18,6 @@ public class WordDictionary {
         node.isWord = true;
     }
 
-    // Returns if the word is in the data structure. A word could
-    // contain the dot character '.' to represent any one letter.
     public boolean search(String word) {
         return match(word.toCharArray(), 0, root);
     }
