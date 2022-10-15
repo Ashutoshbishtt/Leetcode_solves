2189058077a697e50dd0395b5046ada8c52d14b4
Ashutoshbishtt <81284766+Ashutoshbishtt@users.noreply.github.com>
2022-10-15 23:12:33 +0530

    Time: 51 ms (62.64%), Space: 43 MB (60.34%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.java
+++ 1531-string-compression-ii/1531-string-compression-ii.java
@@ -1,29 +1,51 @@
 class Solution {
 
     public int getLengthOfOptimalCompression(String s, int k) {
-        int n = s.length();
-        int[][] dp = new int[110][110];
-
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= n; j++) dp[i][j] = 9999;
+        int N = s.length();
+        int[][] dp = new int[N+1][k+1];
+        for (int i = 0; i <= N; i++) {
+            Arrays.fill(dp[i], N);
         }
-
         dp[0][0] = 0;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= k; j++) {
-                int cnt = 0, del = 0;
-                for (int l = i; l >= 1; l--) {
-                    if (s.charAt(l - 1) == s.charAt(i - 1)) cnt++; else del++;
-                    if (j - del >= 0) {
-                        dp[i][j] = Math.min(dp[i][j], dp[l - 1][j - del] + 1 + (cnt >= 100 ? 3 : cnt >= 10 ? 2 : cnt >= 2 ? 1 : 0));
-                    }
+        for (int i = 1; i <= N; i++) {
+            for (int kk = 0; kk <= Math.min(i, k); kk++) {
+                if (kk-1 >= 0) {
+                    dp[i][kk] = dp[i-1][kk-1]; 
                 }
-                if(j>0){
-                    dp[i][j] = Math.min(dp[i][j],dp[i-1][j-1]);
+                
+
+                char cur = s.charAt(i-1);
+
+                int maxDel = kk; 
+                int curCharCount = 0;
+                for (int j = i-1; j >= 0; j--) {
+                    if (s.charAt(j) == cur) {
+                        curCharCount++;
+                    } else {
+                        maxDel--;
+                    }
+
+                    if (maxDel < 0) {
+                        break;
+                    }
+
+                    dp[i][kk] = Math.min(dp[i][kk], dp[j][maxDel] + getLen(curCharCount));
                 }
             }
         }
-        return dp[n][k];
+
+        return dp[N][k];
+    }
+
+    private int getLen(int curCharCount) {
+        if (curCharCount == 1) {
+            return 1;
+        } else if (curCharCount < 10) {
+            return 2;
+        } else if (curCharCount < 100) {
+            return 3;
+        } else {
+            return 4;
+        }
     }
 }
