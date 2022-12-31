107d79ed6172d3c9873dd6307c2357e7db9bf979
Ashutoshbishtt <81284766+Ashutoshbishtt@users.noreply.github.com>
2022-12-31 21:07:29 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (82.79%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.java
+++ 0980-unique-paths-iii/0980-unique-paths-iii.java
@@ -1,41 +1,37 @@
 class Solution {
+
     public int uniquePathsIII(int[][] grid) {
         int zero = 0;
         int x = 0;
         int y = 0;
-        
-        for(int i = 0 ; i < grid.length ; i++){
-            for(int j = 0 ; j < grid[0].length ; j++){
-                if(grid[i][j] == 0) zero++;
-                else if(grid[i][j] == 1){
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 0) zero++; else if (grid[i][j] == 1) {
                     x = i;
                     y = j;
                 }
             }
         }
-        
-       return dfs(grid,x,y,zero);
-        
+
+        return dfs(grid, x, y, zero);
     }
-    
-    private int dfs(int grid[][] , int x , int y , int zero){
-        if(x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] == -1){
+
+    private int dfs(int grid[][], int x, int y, int zero) {
+        if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] == -1) {
             return 0;
         }
-        
-        if(grid[x][y]==2){
+
+        if (grid[x][y] == 2) {
             return zero == -1 ? 1 : 0;
         }
-        
+
         grid[x][y] = -1;
         zero--;
-        
-        int totalPaths = dfs(grid,x+1,y,zero)+
-            dfs(grid,x-1,y,zero)+
-            dfs(grid,x,y+1,zero)+
-            dfs(grid,x,y-1,zero);
-        
-        grid[x][y]=0;
+
+        int totalPaths = dfs(grid, x + 1, y, zero) + dfs(grid, x - 1, y, zero) + dfs(grid, x, y + 1, zero) + dfs(grid, x, y - 1, zero);
+
+        grid[x][y] = 0;
         zero++;
         return totalPaths;
     }
