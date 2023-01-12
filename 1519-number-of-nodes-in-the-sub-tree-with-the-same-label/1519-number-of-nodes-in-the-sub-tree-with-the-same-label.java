5fd1281af102c6fc49c5e6fc314c0ce979fb9c54
Ashutoshbishtt <81284766+Ashutoshbishtt@users.noreply.github.com>
2023-01-13 00:09:09 +0530

    Time: 105 ms (84.78%), Space: 144.7 MB (66.30%) - LeetHub

---
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.java
@@ -0,0 +1,39 @@
+class Solution {
+    private int[] ans;
+    private List<List<Integer>> adjList;
+    private Set<Integer> visited;
+
+    public int[] countSubTrees(int n, int[][] edges, String labels) {
+        ans = new int[n];
+
+        adjList = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+        for (int[] e : edges) {
+            adjList.get(e[0]).add(e[1]);
+            adjList.get(e[1]).add(e[0]);
+        }
+        visited = new HashSet<Integer>(n);
+        dfs(0, labels);
+        return ans;
+    }
+
+    private int[] dfs(int node, String labels) {
+        visited.add(node);
+        int[] count = new int[26];
+        for (int adj : adjList.get(node)) {
+            if (!visited.contains(adj)) {
+                int[] adjCount = dfs(adj, labels);
+                for (int i = 0; i < 26; i++) {
+                    count[i] += adjCount[i];
+                }
+            }
+        }
+        char ch = labels.charAt(node);
+        count[ch - 'a']++;
+
+        ans[node] = count[ch - 'a'];
+        return count;
+    }
+}
