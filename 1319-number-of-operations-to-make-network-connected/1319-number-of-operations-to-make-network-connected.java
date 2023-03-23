7d1a29b9992f4d5e94c11dad64439ca3cc9037fd
Ashutosh Bisht <81284766+Ashutoshbishtt@users.noreply.github.com>
2023-03-23 18:44:49 +0530

    Time: 15 ms (49.84%), Space: 60 MB (67.26%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,35 +1,33 @@
 class Solution {
+
     public int makeConnected(int n, int[][] connections) {
-        if(connections.length<n-1)return -1;
-        
+        if (connections.length < n - 1) return -1;
+
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i = 0 ; i < n ; i++)
-            adj.add(new ArrayList<>());
-        for(int[] edge : connections){
+        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
+        for (int[] edge : connections) {
             adj.get(edge[0]).add(edge[1]);
             adj.get(edge[1]).add(edge[0]);
         }
-        
+
         boolean[] visited = new boolean[n];
-        
+
         int count = 0;
-        
-        for(int i = 0 ; i < n ; i++){
-            if(!visited[i]){
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
                 count++;
-                dfs(adj,i,visited);
+                dfs(adj, i, visited);
             }
         }
-        
-        return count-1;
+
+        return count - 1;
     }
-    
-    void dfs(ArrayList<ArrayList<Integer>>adj , int node , boolean[] visited){        
+
+    void dfs(ArrayList<ArrayList<Integer>> adj, int node, boolean[] visited) {
         visited[node] = true;
-        for(int i : adj.get(node)){
-            if(!visited[i])
-                dfs(adj,i,visited);
+        for (int i : adj.get(node)) {
+            if (!visited[i]) dfs(adj, i, visited);
         }
     }
-    
 }
