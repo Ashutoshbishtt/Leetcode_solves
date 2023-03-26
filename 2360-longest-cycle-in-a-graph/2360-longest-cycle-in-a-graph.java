b26cac095ec6fd3968e36006d2b7e7fb2f2d8f9e
Ashutosh Bisht <81284766+Ashutoshbishtt@users.noreply.github.com>
2023-03-26 21:30:40 +0530

    Time: 37 ms (71.81%), Space: 62.9 MB (64.48%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
@@ -2,44 +2,40 @@ class Solution {
     int longestCycleLength = -1;
     int[] nodeVisitedInCycle;
     int currCycle = 1;
-    
+
     public int longestCycle(int[] edges) {
-        
         int n = edges.length;
         boolean[] visited = new boolean[n];
         int[] timeNodeWasVisitedInCurrentTraversal = new int[n];
         nodeVisitedInCycle = new int[n];
-        
+
         for (int node = 0; node < n; ++node) {
             traverseNode(edges, node, visited, timeNodeWasVisitedInCurrentTraversal, 1);
             ++currCycle;
         }
-        
+
         return longestCycleLength;
     }
-    
+
     public void traverseNode(int[] edges, int node, boolean[] visited, int[] timeNodeWasVisitedInCurrentTraversal, int currTime) {
-        
         if (node == -1) {
             return;
         }
-        
+
         if (visited[node]) {
-            if (nodeVisitedInCycle[node] != currCycle) {     
+            if (nodeVisitedInCycle[node] != currCycle) {
                 return;
-            }
-            
-            else {     
+            } else {
                 int visitedTime = timeNodeWasVisitedInCurrentTraversal[node];
                 longestCycleLength = Math.max(longestCycleLength, currTime - visitedTime);
                 return;
             }
         }
-        
+
         visited[node] = true;
         nodeVisitedInCycle[node] = currCycle;
         timeNodeWasVisitedInCurrentTraversal[node] = currTime;
-        
+
         traverseNode(edges, edges[node], visited, timeNodeWasVisitedInCurrentTraversal, currTime + 1);
     }
 }
