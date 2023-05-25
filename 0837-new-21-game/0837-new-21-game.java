8ec18bb51703517a8a0727249d2e5d6ae227ff07
Ashutosh Bisht <81284766+Ashutoshbishtt@users.noreply.github.com>
2023-05-25 22:09:12 +0530

    Time: 4 ms (34.94%), Space: 43.2 MB (7.23%) - LeetHub

---
+++ 0837-new-21-game/0837-new-21-game.java
@@ -0,0 +1,25 @@
+class Solution {
+    public double new21Game(int n, int k, int maxPts) {
+
+        if (k == 0 || n >= k - 1 + maxPts)
+          return 1.0;
+
+        double ans = 0.0;
+        double[] dp = new double[n + 1];
+        dp[0] = 1.0;
+        double windowSum = dp[0]; 
+
+        for (int i = 1; i <= n; ++i) {
+
+          dp[i] = windowSum / maxPts;
+          if (i < k)
+            windowSum += dp[i];
+          else 
+            ans += dp[i];
+          if (i - maxPts >= 0)
+            windowSum -= dp[i - maxPts];
+        }
+
+        return ans;
+    }
+}
